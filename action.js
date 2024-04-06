9a4a8bee81acf24884603b9c82c7c8e360c92f83
Mohan Raj M <146982662+the1percen@users.noreply.github.com>
2024-04-06 15:20:40 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,19 @@
+document.getElementById
+("contact-form").addEventListener
+("submit", function(event){
+    event.preventDefault();
+    var data = new FormData(
+        event,target);
+    fetch("",{
+        method: 'POST',
+        body: data,
+    }).then(response => {
+      if (response.ok){
+        alert('Success');
+      } else{
+        alert('error');
+      }
+    });
+    document.getElementById
+    ('contact-form').reset();
+});
